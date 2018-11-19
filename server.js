2a497b0d730f90d1c20e3290e892c1d90c5a0c99
Minseok <iamminseokpark@gmail.com>
2018-11-20 08:00:25 +0900

    서버 라우팅 설정 및 에셋 정리

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 
 /* Socket.io 초기화 */
 const express = require('express');
-const socketIO = require('socket.io');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const sokectConnector = require('./socket-connector.js');
+
 
 const app = express();
 const server = require('http').createServer(app);
@@ -13,47 +15,35 @@ app.use(express.static(path.join(__dirname, '.')));
 
 server.listen(port, () => console.log('>>> http://localhost:' + port));
 
-//app.get('/', function(req, res) {
-//    res.sendFile(__dirname + '/client-index.html')
-//})
-//
-//app.get('/manage', function(req, res) {
-//    res.sendFile(__dirname + '/manager-index.html')
-//})
+/**
+*   Socket Connector.
+*   Socket과 관련된 모든 행동을 수행 함.
+*/
+sokectConnector.initSocket(server);   
+
+var name = "";
+
+/* URL */
+app.get('/client', function(req, res) {
+    res.sendFile(__dirname + '/chat/client-index.html');
+})
+
+app.get('/manage', function(req, res) {
+    res.sendFile(__dirname + '/chat/manager-index.html');
+})
+
+
+app.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/singin/index.html');
+})
+
+app.get('/newchannel', function(req, res) {
+    res.sendFile(__dirname + '/channel/index.html');
+})
+
+app.get('/problems', function(req, res) {
+    res.sendFile(__dirname + '/problems/index.html');
+})
+
 
 //app.use(express.static(path.join(__dirname, 'css/styles.css')));
-
-const io = socketIO(server);
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.emit('connection', "Connected.");
-    
-
-    socket.on('code', function(data) {
-//        console.log("CodeUpdate!: " + data);
-        
-        /* TODO */
-        // socket.of(roomnum).emit 형태로 변경
-        socket.broadcast.emit('codeUpdate', data);
-    });
-
-    /**
-    *   도움 요청
-    */
-    socket.on('help', (data) => {
-        console.log("Somebody wants help!")
-        socket.broadcast.emit('somebodyHelp', data)
-    });
-
-    /**
-    *   도움 요청
-    */
-    socket.on('submit', (data) => {
-        console.log("Somebody submits!")
-
-        socket.broadcast.emit('somebodySubmit', data)
-    });
-
-    /* 웹페이지를 닫았을때 뒤 함수 호출*/
-    socket.on('disconnect', () => console.log('Client disconnected'));
-});
