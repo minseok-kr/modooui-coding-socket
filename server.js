8a4fc2c62b744c5944208fe88b264d47cc33a561
Minseok <iamminseokpark@gmail.com>
2018-11-21 13:44:52 +0900

    py파일 컴파일 기능 추가
    - py파일에 대한 빌드, 컴파일 기능 추가
    - 빌드 결과에 따른 Toast 메시지 추가

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 'use strict';
 
 /* Socket.io 초기화 */
+const fs = require('fs');
+
 const express = require('express');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const sokectConnector = require('./socket-connector.js');
 
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'site')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 server.listen(port, () => console.log('>>> http://localhost:' + port));
 
@@ -48,5 +53,50 @@ app.get('/problems', function(req, res) {
     res.sendFile(siteurl + 'problems/index.html');
 })
 
+var exec = require('child_process').exec, child;
+
+app.post('/checkcode', function(req,res) {
+    console.log("someone try to compile code.")
+    console.log(req.body);
+    let code = req.body.code;
+    let filename = "text.py"
+
+    let encoded = "#-*-coding:utf-8\n"
+
+    // 파일 만들고.
+    fs.writeFile("tmp/" + filename, encoded + code, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("File was saved.")
+
+        // 컴파일.
+        let opts = {
+            encoding: 'utf8',
+            timeout: 2000,
+            maxBuffer: 10*1024,
+            killSignal: 'SIGTERM',
+            cwd: null,
+            env: null
+        }
+
+        child = exec("python tmp/" + filename, opts, function(error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+                console.log("exec error: " + error);
+            }
+
+            let status = ""
+            if (error == null) {
+                status = "빌드 성공"    
+            } else {
+                status = "빌드 실패"
+            }
+
+            res.json({ status: status, stdout: stdout, stderr: stderr, error: error });
+        })
+    })
+})
 
 //app.use(express.static(path.join(__dirname, 'css/styles.css')));
