7881f0ebd07b124d2d5d2fcf8823bc336ea96147
Minseok <iamminseokpark@gmail.com>
2018-11-25 00:42:51 +0900

    Revert "Merge branch 'master' of https://github.com/minseok-kr/modooui-coding-socket"
    This reverts commit 77067cb5ea9ec91d1687f1a9e13acb0dae7dfa48, reversing
    changes made to 91f82d7bb392f6ed805c453ad7cd6c33dcc39210.

--- module/eventer.js
+++ module/eventer.js
@@ -1,11 +1,11 @@
-const socketIO = require('socket.io');
+/**
+*   사용자에게 알림을 담당하는 모듈
+*/
 
-exports.initSocket = function(server) {
-    const io = socketIO(server);
+exports.onEvent = function(io) {
     io.on('connection', (socket) => {
         console.log('Client connected');
         socket.emit('connection', "Connected.");
-
 
         socket.on('code', function(data) {
             //        console.log("CodeUpdate!: " + data);
@@ -16,24 +16,30 @@ exports.initSocket = function(server) {
         });
 
         /**
-    *   도움 요청
-    */
+        *   도움 요청
+        */
         socket.on('help', (data) => {
             console.log("Somebody wants help!")
             socket.broadcast.emit('somebodyHelp', data)
         });
 
         /**
-    *   도움 요청
-    */
+        *   제출 요청
+        */
         socket.on('submit', (data) => {
             console.log("Somebody submits!")
 
             socket.broadcast.emit('somebodySubmit', data)
         });
+        
+        
+        socket.on('submitAnswer', (data) => {
+            console.log("Submit but Awnser is " + data);
+            
+            socket.broadcast.emit('submitResult', data);
+        });
 
         /* 웹페이지를 닫았을때 뒤 함수 호출*/
         socket.on('disconnect', () => console.log('Client disconnected'));
-    });    
+    })
 }
-
