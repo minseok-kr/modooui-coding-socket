5c743165dfd942a72c1188792c0fe9c07afa3cad
Minseok <iamminseokpark@gmail.com>
2018-12-08 17:27:19 +0900

    사용하지 않는 파일 정리

--- ChatExample 복사본/passport/local_login.js
+++
@@ -1,9 +0,0 @@
-var LocalStrategy=require('passport-local').Strategy;
-
-module.exports=new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-}, function(req, email, password, done){
-    console.log('passport의 local-login 호출됨' + email + ',' + password );
-});
--- ChatExample 복사본/routes/test.js
+++
@@ -1,3 +0,0 @@
-var user= require('./user');
-
-//user.printuser();
